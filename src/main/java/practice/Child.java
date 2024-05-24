94c1158c0e6b52fa8db7c59e4e39bfc9b2726ba7
hemanthcse1 <hemanthcse1@gmail.com>
2024-05-24 20:54:51 +0530

    1. Minimum size subarray size leetcode

---
+++ src/main/java/practice/Child.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Child extends Parent{
+
+    Child(){
+        super();
+        System.out.println("child class");
+    }
+}
---
+++ src/main/java/practice/MainPractice.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class MainPractice {
+
+    public static void main(String[] args){
+        Child child = new Child();
+    }
+}
