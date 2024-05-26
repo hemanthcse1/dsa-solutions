432ddd18508e5112168889f1fd81d3d070f73e8a
hemanthcse1 <hemanthcse1@gmail.com>
2024-05-26 11:11:00 +0530

    1. Single Non Repeating number in a give array of integers

--- src/main/java/problems/arrays/singlenonrepeatingnumber/SingleNonRepeatingNumber.java
+++ src/main/java/problems/arrays/singlenonrepeatingnumber/SingleNonRepeatingNumber.java
@@ -3,20 +3,20 @@ package problems.arrays.singlenonrepeatingnumber;
 public class SingleNonRepeatingNumber {
 
     public static void main(String[] args){
-        int[] arr = {4,1,2,1,2};
+        int[] nums = {4,1,2,1,2};
 
 
-        System.out.print("Single no repeating number is -> "+findSingleNonRepeatingNumber(arr));
+        System.out.print("Single no repeating number is -> "+findSingleNonRepeatingNumber(nums));
 
     }
 
-    public static int findSingleNonRepeatingNumber(int[] arr){
+    public static int findSingleNonRepeatingNumber(int[] nums){
 
 
         int number = 0;
-        for (int i = 0; i< arr.length; i++){
+        for (int i = 0; i< nums.length; i++){
 
-            number = number ^ arr[i];
+            number = number ^ nums[i];
         }
 
         return number;
